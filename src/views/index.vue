9ca56f525b230dd97b056d55e3706e5afa209e63
jonaaldas <jonaaldas@gmail.com>
2024-01-07 15:58:39 -0500

    starter files

---
+++ src/views/index.vue
@@ -0,0 +1,93 @@
+<template>
+	<main class="h-screen">
+		<div class="flex flex-col items-center m-6 p-3">
+			<div class="flex flex-col justify-center items-center w-7/12">
+				<img src="../assets/img/home.jpg" alt="" />
+				<div class="w-full bg-cardBackground p-3">
+					<h1>ENGLISH</h1>
+					<h1>ITALIANO</h1>
+					<h1>UKRANIAN</h1>
+				</div>
+			</div>
+		</div>
+		<div class="grid grid-cols-2 gap-4">
+			<div class="grid grid-cols-1 auto-rows-auto space-y-5">
+				<div class="bg-cardBackground p-3">
+					<a href="#">
+						<p>LINKS</p>
+					</a>
+					<a href="#">
+						<p>RESUME</p>
+					</a>
+					<a href="#">
+						<p>PORTFOLIO</p>
+					</a>
+					<a href="#">
+						<p>GENOA</p>
+					</a>
+					<a href="#">
+						<p>BAUHAUS</p>
+					</a>
+					<a href="#">
+						<p>MIAMI</p>
+					</a>
+					<a href="#">
+						<p>ISTANBUL</p>
+					</a>
+					<a href="#">
+						<p>KYIV</p>
+					</a>
+					<a href="#">
+						<p>ART</p>
+					</a>
+				</div>
+				<div class="bg-cardBackground p-3 h-[80px]">
+					<a href="#">
+						<p>CONTACT ME</p>
+					</a>
+				</div>
+				<div class="bg-cardBackground p-3">
+					<a href="#">
+						<p>GET TO KNOW ME</p>
+					</a>
+					<a href="#">
+						<p>I AM A UKRAINIAN</p>
+					</a>
+					<a href="#">
+						<p>LIVING IN AMERICA</p>
+					</a>
+					<a href="#">
+						<p>PAINTER, ARTIST, SCULPTURE</p>
+					</a>
+					<a href="#">
+						<p>PASSIONATE ABOUT POLITICS</p>
+					</a>
+				</div>
+			</div>
+			<div class="grid grid-cols-1 auto-rows-auto space-y-5">
+				<div class="bg-cardBackground p-3 space-y-3">
+					<p class="text-xs">PROFESSIONAL BACKGROUND</p>
+					<ul>
+						<li class="text-xs">- MASTERS OF ARCHITECTURE</li>
+						<li class="text-xs">- ARCHITECTURAL DESIGNER</li>
+						<li class="text-xs">- LANDSCAPE DESIGNER</li>
+						<li class="text-xs">- ISA CERTIFIED ARBORIST</li>
+						<li class="text-xs">- OWNER OF ONE TREE ARBORIST LLC</li>
+					</ul>
+				</div>
+				<div class="bg-cardBackground p-3">
+					<p>DONATE WITH ME TO UKRAINIAN ARMED FORCES</p>
+					<a href="#"><p>SAVEALIFE.UA</p></a>
+				</div>
+				<div class="bg-cardBackground p-3">
+					<p>PROJECTS</p>
+					<div>
+						<img src="../assets/img/home.jpg" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+<script setup lang="ts"></script>
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		colors: {
+			cardBackground: '#A9A9A9' //dark grey
+		},
+		extend: {}
+	},
+	plugins: []
+};

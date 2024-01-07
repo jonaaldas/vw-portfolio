24c2d6cf8045da8a820698ccd98948fb891b2b5a
jonaaldas <jonaaldas@gmail.com>
2024-01-07 16:21:15 -0500

    finished index mobile

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<main class="h-screen">
+	<main class="h-screen p-4">
 		<div class="flex flex-col items-center m-6 p-3">
 			<div class="flex flex-col justify-center items-center w-7/12">
 				<img src="../assets/img/home.jpg" alt="" />
@@ -16,10 +16,10 @@
 					<a href="#">
 						<p>LINKS</p>
 					</a>
-					<a href="#">
+					<a href="#" class="mt-3 block">
 						<p>RESUME</p>
 					</a>
-					<a href="#">
+					<a href="#" class="block mb-3">
 						<p>PORTFOLIO</p>
 					</a>
 					<a href="#">
@@ -43,31 +43,25 @@
 				</div>
 				<div class="bg-cardBackground p-3 h-[80px]">
 					<a href="#">
-						<p>CONTACT ME</p>
+						<p class="text-xs">CONTACT ME</p>
 					</a>
 				</div>
 				<div class="bg-cardBackground p-3">
-					<a href="#">
-						<p>GET TO KNOW ME</p>
-					</a>
-					<a href="#">
-						<p>I AM A UKRAINIAN</p>
-					</a>
-					<a href="#">
-						<p>LIVING IN AMERICA</p>
-					</a>
-					<a href="#">
-						<p>PAINTER, ARTIST, SCULPTURE</p>
-					</a>
-					<a href="#">
-						<p>PASSIONATE ABOUT POLITICS</p>
-					</a>
+					<p class="text-xs">GET TO KNOW ME</p>
+
+					<p class="text-xs">I AM A UKRAINIAN</p>
+
+					<p class="text-xs">LIVING IN AMERICA</p>
+
+					<p class="text-xs">PAINTER, ARTIST, SCULPTURE</p>
+
+					<p class="text-xs">PASSIONATE ABOUT POLITICS</p>
 				</div>
 			</div>
 			<div class="grid grid-cols-1 auto-rows-auto space-y-5">
 				<div class="bg-cardBackground p-3 space-y-3">
 					<p class="text-xs">PROFESSIONAL BACKGROUND</p>
-					<ul>
+					<ul class="text-xs">
 						<li class="text-xs">- MASTERS OF ARCHITECTURE</li>
 						<li class="text-xs">- ARCHITECTURAL DESIGNER</li>
 						<li class="text-xs">- LANDSCAPE DESIGNER</li>
@@ -76,8 +70,8 @@
 					</ul>
 				</div>
 				<div class="bg-cardBackground p-3">
-					<p>DONATE WITH ME TO UKRAINIAN ARMED FORCES</p>
-					<a href="#"><p>SAVEALIFE.UA</p></a>
+					<p class="text-xs">DONATE WITH ME TO UKRAINIAN ARMED FORCES</p>
+					<a href="#"><p class="text-xs">SAVEALIFE.UA</p></a>
 				</div>
 				<div class="bg-cardBackground p-3">
 					<p>PROJECTS</p>

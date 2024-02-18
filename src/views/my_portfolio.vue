1364e3149b200b403071029784bb30189336b7ea
jonaaldas <jonaaldas@gmail.com>
2024-02-17 22:39:48 -0500

    fix link

--- src/views/my_portfolio.vue
+++ src/views/my_portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-	<div><embed style="width: 100vw; height: 100vh" src="https://vwillard.s3.amazonaws.com/portfolio" /></div>
+	<div><embed style="width: 100vw; height: 100vh" src="https://vwillard.s3.amazonaws.com/PORTFOLIO+2025.pdf" /></div>
 </template>
 
 <script setup lang="ts"></script>

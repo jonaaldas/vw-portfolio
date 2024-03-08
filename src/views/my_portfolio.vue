47acd282bc69d55d2f298f684246fbea4f49c910
jonaaldas <jonaaldas@gmail.com>
2024-03-08 15:42:06 -0500

    new link

--- src/views/my_portfolio.vue
+++ src/views/my_portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-	<div><embed style="width: 100vw; height: 100vh" src="https://vwillard.s3.amazonaws.com/PORTFOLIO+2025.pdf" /></div>
+	<div><embed style="width: 100vw; height: 100vh" src="https://vwillard.s3.amazonaws.com/PORTFOLIO+2024+11x17.pdf" /></div>
 </template>
 
 <script setup lang="ts"></script>

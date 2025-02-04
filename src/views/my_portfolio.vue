f8c24af10d96769cc21ae2c871b56236b18969c9
Jonathan Aldas <jonaaldas@gmail.com>
2025-02-04 17:20:44 +0100

    new-link

--- src/views/my_portfolio.vue
+++ src/views/my_portfolio.vue
@@ -1,60 +1,11 @@
 <template>
   <div>
-    <iframe v-if="currentChunk" :src="currentChunk" class="w-full h-screen"></iframe>
+    <iframe
+      src="https://k2v5aihjnd.ufs.sh/f/YlnYmwr2Dbqp9oNYxCU3Sye4fhdA91sC0NLazVFKwvRg5jbU"
+      class="w-full h-screen"></iframe>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted, onUnmounted } from "vue";
-
-const currentChunk = ref(null);
-const progress = ref(0);
-
-const loadPDF = async () => {
-  const pdfUrl = "https://utfs.io/f/C8qVjzdwVfIiGutkVrAfrZGj76QWc0D2MCPTXpwtx98VORUk";
-  try {
-    const response = await fetch(pdfUrl);
-    const reader = response.body.getReader();
-    const contentLength = response.headers.get("content-length");
-
-    let receivedLength = 0;
-    const chunks = [];
-
-    while (true) {
-      try {
-        const { done, value } = await reader.read();
-        if (done) break;
-
-        chunks.push(value);
-        receivedLength += value.length;
-        progress.value = Math.round((receivedLength / contentLength) * 100);
-
-        if (currentChunk.value) {
-          URL.revokeObjectURL(currentChunk.value);
-        }
-        const blob = new Blob(chunks, { type: "application/pdf" });
-        currentChunk.value = URL.createObjectURL(blob);
-      } catch (err) {
-        console.error("Chunk read error:", err);
-        break;
-      }
-    }
-  } catch (err) {
-    console.error("PDF load error:", err);
-  }
-};
-
-const initLoad = () => {
-  loadPDF().catch((err) => console.error("Init error:", err));
-};
-
-onMounted(initLoad);
-
-onUnmounted(() => {
-  if (currentChunk.value) {
-    URL.revokeObjectURL(currentChunk.value);
-  }
-});
-</script>
+<script setup></script>
 
 <style scoped></style>

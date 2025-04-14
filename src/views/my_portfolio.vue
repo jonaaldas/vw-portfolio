5d904f9a7e62b27b801c38b7e77a56ef9fee8de5
Jonathan Aldas <jonaaldas@gmail.com>
2025-04-14 13:49:47 +0200

    new google doc v

--- src/views/my_portfolio.vue
+++ src/views/my_portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="portfolio-container">
     <iframe
-      src="https://k2v5aihjnd.ufs.sh/f/YlnYmwr2Dbqp9oNYxCU3Sye4fhdA91sC0NLazVFKwvRg5jbU"
+      src="https://docs.google.com/viewer?url=https://k2v5aihjnd.ufs.sh/f/YlnYmwr2Dbqp9oNYxCU3Sye4fhdA91sC0NLazVFKwvRg5jbU&embedded=true"
       class="w-full h-screen iframe-content"
       scrolling="yes"
       allow="autoplay"

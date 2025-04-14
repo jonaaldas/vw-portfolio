276a3177e1b75c5c531f204ac00f1257ef4bbf2b
Jonathan Aldas <jonaaldas@gmail.com>
2025-04-14 13:35:18 +0200

    fix mobile

--- src/views/my_portfolio.vue
+++ src/views/my_portfolio.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
+  <div class="portfolio-container">
     <iframe
       src="https://k2v5aihjnd.ufs.sh/f/YlnYmwr2Dbqp9oNYxCU3Sye4fhdA91sC0NLazVFKwvRg5jbU"
-      class="w-full h-screen"></iframe>
+      class="w-full h-screen overflow-auto"
+    ></iframe>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.portfolio-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  -webkit-overflow-scrolling: touch; /* For better scrolling on iOS devices */
+}
+
+@media (max-width: 768px) {
+  iframe {
+    height: 100% !important;
+    min-height: 100vh;
+    overflow-y: scroll;
+  }
+}
+</style>
